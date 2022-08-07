8c5cbcfcc0c21e2aa6c2c9ee62fa646dce633061
Antony Koo <kina5418@naver.com>
2022-08-08 02:38:27 +0900

    수정하다가 뒤로 미룸

--- src/main/java/com/example/intermediate/controller/response/PostResponseDto.java
+++ src/main/java/com/example/intermediate/controller/response/PostResponseDto.java
@@ -2,6 +2,8 @@ package com.example.intermediate.controller.response;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import com.example.intermediate.domain.Post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,14 +11,15 @@ import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class PostResponseDto {
   private Long id;
   private String title;
-  private String content;
+  private String content;  // 추후 수정사항 => 게시글 전체 조회에서는 글제목만 보여줘야 함
   private String author;
   private List<CommentResponseDto> commentResponseDtoList;
-  private LocalDateTime createdAt;
+  private LocalDateTime createdAt;  // 추후 수정사항2 => 사실 createdAt으로 조회하되 modified로 바뀔 수 있어야 함
   private LocalDateTime modifiedAt;
+
 }
