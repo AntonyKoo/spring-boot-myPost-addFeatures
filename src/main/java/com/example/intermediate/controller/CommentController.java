3be608760028913be60b66c94ff5a19896e99e0d
Antony Koo <kina5418@naver.com>
2022-08-08 02:38:04 +0900

    컨트롤러 내 각 기능에 한글 추가

--- src/main/java/com/example/intermediate/controller/CommentController.java
+++ src/main/java/com/example/intermediate/controller/CommentController.java
@@ -19,23 +19,27 @@ public class CommentController {
 
   private final CommentService commentService;
 
+  // 댓글 작성
   @RequestMapping(value = "/api/auth/comment", method = RequestMethod.POST)
   public ResponseDto<?> createComment(@RequestBody CommentRequestDto requestDto,
       HttpServletRequest request) {
     return commentService.createComment(requestDto, request);
   }
 
+
   @RequestMapping(value = "/api/comment/{id}", method = RequestMethod.GET)
   public ResponseDto<?> getAllComments(@PathVariable Long id) {
     return commentService.getAllCommentsByPost(id);
   }
 
+  // 댓글 수정
   @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.PUT)
   public ResponseDto<?> updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto,
       HttpServletRequest request) {
     return commentService.updateComment(id, requestDto, request);
   }
 
+  // 댓글 삭제
   @RequestMapping(value = "/api/auth/comment/{id}", method = RequestMethod.DELETE)
   public ResponseDto<?> deleteComment(@PathVariable Long id,
       HttpServletRequest request) {
