f9f7cdfa7901fc6408be0313da2cb49752e2db88
Antony Koo <kina5418@naver.com>
2022-08-08 19:40:57 +0900

    좋아요 필드

--- src/main/java/com/example/intermediate/domain/ReComment.java
+++ src/main/java/com/example/intermediate/domain/ReComment.java
@@ -21,7 +21,7 @@ public class ReComment extends Timestamped {
 
     @Column(nullable = false)
     private String content;
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id", nullable = false)
     private Comment comment;
