fc1c6994b33d6a558fb86778ee545362d53648e8
Antony Koo <kina5418@naver.com>
2022-08-09 06:14:13 +0900

    modifying ReCommentRepo

--- src/main/java/com/example/intermediate/repository/ReCommentRepository.java
+++ src/main/java/com/example/intermediate/repository/ReCommentRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReCommentRepository extends JpaRepository<ReComment,Long> {
-    List<ReComment> findAllByComment(Comment comment);
+    List<ReComment> findAllByCommentId(Long commentId);
 }
