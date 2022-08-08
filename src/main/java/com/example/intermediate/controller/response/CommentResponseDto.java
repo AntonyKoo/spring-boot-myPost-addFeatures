06209df67af13a29d13688b951dbafd67a8ae1d5
Antony Koo <kina5418@naver.com>
2022-08-08 19:37:40 +0900

    no message

--- src/main/java/com/example/intermediate/controller/response/CommentResponseDto.java
+++ src/main/java/com/example/intermediate/controller/response/CommentResponseDto.java
@@ -1,6 +1,9 @@
 package com.example.intermediate.controller.response;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.intermediate.domain.ReComment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +17,7 @@ public class CommentResponseDto {
   private Long id;
   private String author;
   private String content;
+  private List<ReCommentResponseDto> reCommentResponseDtoList;
   private LocalDateTime createdAt;
   private LocalDateTime modifiedAt;
 }
