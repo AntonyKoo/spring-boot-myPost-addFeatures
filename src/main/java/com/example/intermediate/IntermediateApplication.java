339b8682406b7b9b75c2e67b7c7a1cc2de2caf91
Antony Koo <kina5418@naver.com>
2022-08-09 11:11:31 +0900

    스케줄러를 추가하였습니다.

--- src/main/java/com/example/intermediate/IntermediateApplication.java
+++ src/main/java/com/example/intermediate/IntermediateApplication.java
@@ -3,7 +3,9 @@ package com.example.intermediate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling  // 스케줄러 관련
 @EnableJpaAuditing
 @SpringBootApplication
 public class IntermediateApplication {
